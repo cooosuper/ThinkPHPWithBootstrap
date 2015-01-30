56e144cf228319c007a75fcaab96cb9ce5ab3663
cooosuper <cooosuper@gmail.com>
2015-01-30 10:56:17 +0800

    提交项目框架

---
+++ code/www/index.php
@@ -0,0 +1,6 @@
+<?php
+    define('APP_DEBUG',TRUE); // 开启调试模式
+    define('APP_NAME','ThinkPHPWithBootstrap');
+    define('APP_PATH','./ThinkPHPWithBootstrap/');
+    require './ThinkPHP/ThinkPHP.php';
+ 
